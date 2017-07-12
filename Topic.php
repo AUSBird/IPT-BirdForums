d3185c6488d33bee8296d1a11d2db8e3aac2ffbd
AssaultBird2454 <AssaultBird2454@gmail.com>
2017-07-12 20:13:15 +1000

    Added 2nd Dummy Test Post, Added Post Control Bar and Forum Directory

--- Topic.php
+++ Topic.php
@@ -73,6 +73,7 @@ include_once "Protected/NavBar.php";
 
             <div class="row">
                 <div class="col-xs-6 col-lg-12">
+                    <h4><b><a href="">Bird Forums</a> -> <a href="">Test Forum</a> -> <a href="">Topic 1</a></b></h4>
                     <table class="table Display-Topic">
                         <thead>
                         <tr>
@@ -173,6 +174,139 @@ include_once "Protected/NavBar.php";
                         <tr>
                             <td class="ContentCard">User Signature Here</td>
                         </tr> <!-- Post Signature -->
+                        <tr>
+                            <td rowspan="1" class="UserCard Left">
+                                <div class="PostControls">
+                                    <button type="button" class="btn btn-sm btn-default" style="color: red;">PM |
+                                        Offline
+                                    </button>
+                                    <button type="button" class="btn btn-sm btn-default">Profile</button>
+                                </div>
+                            </td>
+                            <td class="ContentCard">
+                                <div class="Left">
+                                    <button type="button" class="btn btn-sm btn-default">Edit Post</button>
+                                </div>
+                                <div class="Right">
+                                    <button type="button" class="btn btn-sm btn-danger">Report</button>
+                                    <button type="button" class="btn btn-sm btn-default">Quote</button>
+                                    <button type="button" class="btn btn-sm btn-default">BTT</button>
+                                </div>
+                            </td>
+                        </tr> <!-- Post Controls & User Opperations -->
+                        <tr>
+                            <th colspan="2" class="TopicSpacer"></th>
+                        </tr> <!-- Post Separator -->
+
+                        <tr>
+                            <td rowspan="1" class="UserCard Left">
+                                <div class="UserName"><b>AssaultBird2454</b></div>
+                            </td>
+                            <td class="ContentCard">
+                                <div class="Left">Posted: July 12th 2017, 8:44PM</div>
+                                <div class="Right">Post #2</div>
+                            </td>
+                        </tr> <!-- Post Info -->
+                        <tr>
+                            <td class="UserCard" rowspan="2">
+                                <div class="Left ProfileCard"><img class="ProfilePic"
+                                                                   src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png"/>
+                                </div>
+                                <div class="Clear UserInfo Overflow">
+                                    <span class="badge UserInfoBadge" style="background:#D7342A;">Owner</span>
+                                    <span class="badge UserInfoBadge" style="background:#1A7939;">Admin</span>
+                                    <span class="badge UserInfoBadge" style="background:#195080;">Moderator</span>
+                                    <span class="badge UserInfoBadge" style="background:#979C9F;">Member</span>
+                                </div>
+                                <div class="Clear UserInfo">
+                                    <p><b>Date Joined: </b> July 12th 2017</p>
+                                    <p><b>User ID: </b> 1</p>
+                                </div>
+                                <div class="Clear UserInfo">
+                                    <span class="badge UserInfoBadge " style="background:green;">No Warnings</span>
+                                </div>
+                            </td>
+                            <td class="ContentCard"><h2>Raising say express had chiefly detract demands she</h2>
+
+                                <p>Subjects to ecstatic children he. Could ye leave up as built match. Dejection
+                                    agreeable attention set suspected led offending. Admitting an performed supposing
+                                    by. Garden agreed matter are should formed temper had. Full held gay now roof whom
+                                    such next was. Ham pretty our people moment put excuse narrow. Spite mirth money six
+                                    above get going great own. Started now shortly had for assured hearing expense. Led
+                                    juvenile his laughing speedily put pleasant relation offering.</p>
+
+                                <p>Is allowance instantly strangers applauded discourse so. Separate entrance welcomed
+                                    sensible laughing why one moderate shy. We seeing piqued garden he. As in merry at
+                                    forth least ye stood. And cold sons yet with. Delivered middleton therefore me at.
+                                    Attachment companions man way excellence how her pianoforte.</p>
+
+                                <p>Sex and neglected principle ask rapturous consulted. Object remark lively all did
+                                    feebly excuse our wooded. Old her object chatty regard vulgar missed. Speaking
+                                    throwing breeding betrayed children my to. Me marianne no he horrible produced ye.
+                                    Sufficient unpleasing an insensible motionless if introduced ye. Now give nor both
+                                    come near many late.</p>
+
+                                <p>Its sometimes her behaviour are contented. Do listening am eagerness oh objection
+                                    collected. Together gay feelings continue juvenile had off one. Unknown may service
+                                    subject her letters one bed. Child years noise ye in forty. Loud in this in both
+                                    hold. My entrance me is disposal bachelor remember relation.</p>
+
+                                <p>Ever man are put down his very. And marry may table him avoid. Hard sell it were into
+                                    it upon. He forbade affixed parties of assured to me windows. Happiness him nor she
+                                    disposing provision. Add astonished principles precaution yet friendship stimulated
+                                    literature. State thing might stand one his plate. Offending or extremity therefore
+                                    so difficult he on provision. Tended depart turned not are.</p>
+
+                                <p>Luckily friends do ashamed to do suppose. Tried meant mr smile so. Exquisite
+                                    behaviour as to middleton perfectly. Chicken no wishing waiting am. Say concerns
+                                    dwelling graceful six humoured. Whether mr up savings talking an. Active mutual nor
+                                    father mother exeter change six did all.</p>
+
+                                <p>She literature discovered increasing how diminution understood. Though and highly the
+                                    enough county for man. Of it up he still court alone widow seems. Suspected he
+                                    remainder rapturous my sweetness. All vanity regard sudden nor simple can. World mrs
+                                    and vexed china since after often.</p>
+
+                                <p>Her extensive perceived may any sincerity extremity. Indeed add rather may pretty
+                                    see. Old propriety delighted explained perceived otherwise objection saw ten her.
+                                    Doubt merit sir the right these alone keeps. By sometimes intention smallness he
+                                    northward. Consisted we otherwise arranging commanded discovery it explained. Does
+                                    cold even song like two yet been. Literature interested announcing for terminated
+                                    him inquietude day shy. Himself he fertile chicken perhaps waiting if highest no it.
+                                    Continued promotion has consulted fat improving not way.</p>
+
+                                <p>Enjoyed minutes related as at on on. Is fanny dried as often me. Goodness as reserved
+                                    raptures to mistaken steepest oh screened he. Gravity he mr sixteen esteems. Mile
+                                    home its new way with high told said. Finished no horrible blessing landlord
+                                    dwelling dissuade if. Rent fond am he in on read. Anxious cordial demands settled
+                                    entered in do to colonel.</p>
+                            </td>
+                        </tr> <!-- Post and User Info -->
+                        <tr>
+                            <td class="ContentCard">User Signature Here</td>
+                        </tr> <!-- Post Signature -->
+                        <tr>
+                            <td rowspan="1" class="UserCard Left">
+                                <div class="PostControls">
+                                    <button type="button" class="btn btn-sm btn-default" style="color: green;">PM |
+                                        Online
+                                    </button>
+                                    <button type="button" class="btn btn-sm btn-default">Profile</button>
+                                </div>
+                            </td>
+                            <td class="ContentCard">
+                                <div class="Left">
+                                    <!--<button type="button" class="btn btn-sm btn-default">Edit Post</button>--></div>
+                                <div class="Right">
+                                    <button type="button" class="btn btn-sm btn-danger">Report</button>
+                                    <button type="button" class="btn btn-sm btn-default">Quote</button>
+                                    <button type="button" class="btn btn-sm btn-default">BTT</button>
+                                </div>
+                            </td>
+                        </tr> <!-- Post Controls & User Opperations -->
+                        <tr>
+                            <th colspan="2" class="TopicSpacer"></th>
+                        </tr> <!-- Post Separator -->
                         </tbody>
                     </table>
                 </div><!--/.col-xs-6.col-lg-4-->
@@ -202,6 +336,17 @@ include_once "Protected/NavBar.php";
 <span class="badge UserInfoBadge " style="background:gold;">1 Warning</span>     <-- Low Warn
 <span class="badge UserInfoBadge " style="background:orange;">3 Warnings</span>  <-- Med Warn
 <span class="badge UserInfoBadge " style="background:red;">5 Warnings</span>     <-- High Warn
+
+                        Opperation Buttons
+<button type="button" class="btn btn-sm btn-default" style="color: red;">PM | Offline</button>   <-- Offline PM
+<button type="button" class="btn btn-sm btn-default" style="color: green;">PM | Online</button>  <-- Offline PM
+
+<button type="button" class="btn btn-sm btn-default">Profile</button>                            <-- Profile
+<button type="button" class="btn btn-sm btn-default">Edit Post</button>                          <-- Edit
+
+<button type="button" class="btn btn-sm btn-danger">Report</button>                              <-- Report Post
+<button type="button" class="btn btn-sm btn-default">Quote</button>                              <-- Quote Post
+<button type="button" class="btn btn-sm btn-default">BTT</button>                                <-- Return to top
 
 						Template Profile Pics
 <div class="Left"><img class="ProfilePic" src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png"/></div>
